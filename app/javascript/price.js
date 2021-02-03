4fac9c8af3afb4b4c6b5cd63f25d784385d10622
mureT-ani <alphons-tomo0218@docom.ne.jp>
2021-02-03 22:23:33 +0900

    販売手数料、利益の計算

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,5 +1,11 @@
 function post (){
-  console.log("test");
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  itemPrice.addEventListener("input", () => {
+    const inputValue = itemPrice.value;
+    addTaxPrice.innerHTML = Math.floor(0.1*inputValue);
+    profit.innerHTML = inputValue - Math.floor(0.1*inputValue)
+  })
 }
-
-window.addEventListener('load', post);
+window.addEventListener('load', post)
