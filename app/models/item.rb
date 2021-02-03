89a732bc16efb7b26ef49ef1c3e18e9b983097ff
mureT-ani <alphons-tomo0218@docom.ne.jp>
2021-02-03 15:52:50 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :days
+  belongs_to :fee
+  belongs_to :status
   belongs_to :user
   has_one_attached :image
+
 end
