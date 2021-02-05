a08dbdf9db77220be2fbb50bae4040840e0d324e
mureT-ani <alphons-tomo0218@docom.ne.jp>
2021-02-05 20:21:29 +0900

    PurchaseRecordPlaceモデルの単体テスト

---
+++ spec/factories/purchase_record_places.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_record_place do
+    postal_number         { '123-4567' }
+    area_id               { Faker::Number.between(from: 2, to: 48) }
+    city                  { Faker::Address.city }
+    house_number          { Faker::Address.street_address }
+    building_name         { Faker::Address.secondary_address }
+    phone_number          { Faker::Number.leading_zero_number(digits: 11)}
+  end
+end
